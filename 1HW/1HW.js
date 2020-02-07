ae1becd89b7e834b7ddf6db88345fdfc873a45be
Oksana Kuliesh <okaresta1@gmail.com>
2020-02-07 17:19:02 +0200

    fixed variables, fixed lostAdvantage

--- 1HW/1HW.js
+++ 1HW/1HW.js
@@ -2,26 +2,26 @@ const price1 = 15.678;
 const price2 = 123.965;
 const price3 = 90.2345;
 
-let maxPrice = Math.max(price1, price2, price3);
+const maxPrice = Math.max(price1, price2, price3);
 console.log('Максимальное значение: ' + maxPrice);
 
-let minPrice = Math.min(price1, price2, price3);
+const minPrice = Math.min(price1, price2, price3);
 console.log('Минимальное значение: ' + minPrice);
 
-let sumPrice = price1 + price2 + price3;
+const sumPrice = price1 + price2 + price3;
 console.log('Сумма значений: ' + sumPrice);
 
 
-let intPrice1 = Math.floor(price1);
-let intPrice2 = Math.trunc(price2);
-let intPrice3 = Math.trunc(price3);
-let intPricesArr = [intPrice1, intPrice2, intPrice3];
+const intPrice1 = Math.floor(price1);
+const intPrice2 = Math.trunc(price2);
+const intPrice3 = Math.trunc(price3);
+const intPricesArr = [intPrice1, intPrice2, intPrice3];
 console.log('Целые значения: ' + intPricesArr);
 
-let intSum = intPrice1 + intPrice2 + intPrice3;
+const intSum = intPrice1 + intPrice2 + intPrice3;
 console.log('Сумма целых: ' + intSum);
 
-let roundIntSum = Math.ceil(intSum * 10 / 1000) * 100;
+const roundIntSum = Math.ceil(intSum * 10 / 1000) * 100;
 console.log ('Округленная сумма целых: ' + roundIntSum);
 
 let isEven = intSum % 2;
@@ -31,23 +31,23 @@ else if (isEven === 0)
   console.log(intSum + ' четное число');
 
 
-let booleanIsEven = (intSum%2) === 0;
+const booleanIsEven = (intSum%2) === 0;
 console.log(booleanIsEven);
 
 const clientsMoney = 500;
-let cashBack = (clientsMoney - sumPrice).toFixed(2);
+const cashBack = (clientsMoney - sumPrice).toFixed(2);
 console.log('Ваша сдача: ' + cashBack + ' грн');
 
-let averagePrice = (sumPrice/3).toFixed(2);
+const averagePrice = (sumPrice/3).toFixed(2);
 console.log('Среднее значение: ' + averagePrice);
 
-let randDiscount = Math.random();
-let discountedPrice = (price2 - randDiscount).toFixed(2);
+const randDiscount = Math.random();
+const discountedPrice = (price2 - randDiscount).toFixed(2);
 console.log(`Congratulations! We have a discount for you! 
              Your old price was: ${price2} 
              Your new price is: ${discountedPrice}`);
 
-let lostAdvantage = (price2/2 - randDiscount).toFixed(2);
+const lostAdvantage = ((sumPrice/2) - (sumPrice * randDiscount)).toFixed(2);
 console.log('Упущенная выгода: ' + lostAdvantage);
 
 console.log(`Advanced:
