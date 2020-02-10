48cb0f75bc900f1323adacd4d8ef27f7b452c4c9
Oksana Kuliesh <okaresta1@gmail.com>
2020-02-10 10:23:07 +0200

    some modifications, game like

--- 2HW/2HW.js
+++ 2HW/2HW.js
@@ -1,20 +1,22 @@
-let startNumber = +prompt('Enter an integer start number, please', '10');
-let finishNumber = +prompt('Enter an integer finish number bigger than start number, please', '100');
+let wannaPlay = confirm('Hey, let\'s play!');
+      if(wannaPlay === true){
+        let startNumber = +prompt('Enter an integer start number, please', '10');
+        let finishNumber = +prompt('Enter an integer finish number bigger than start number, please', '100');
+      if (startNumber && finishNumber !== NaN) {
 let fixedStartNumber = Math.round(startNumber);
 let fixedFinishNumber = Math.round(finishNumber);
-console.log(fixedStartNumber);
-console.log(fixedFinishNumber);
-let passOrNot = confirm('Do you want to pass through even numbers?');
-console.log(passOrNot);
-if (passOrNot === false) {
-  console.log('Don\'t pass!');
+document.writeln(`Your start number is: ${fixedStartNumber}`);
+document.writeln(`Your finish number is: ${fixedFinishNumber}`);
+      let passOrNot = confirm('Do you want to pass through even numbers?');
+      if (passOrNot === false) {
+  document.writeln('Don\'t pass through even numbers!');
   let sum = 0;
   for (let i = fixedStartNumber; i<= fixedFinishNumber; i++) {
     sum+=i;
   }
-  console.log(sum);
+  document.writeln(`Your numbers sum is: ${sum}`);
 } else if (passOrNot === true) {
-  console.log('Let\'s pass!');
+  document.writeln('Let\'s pass through even numbers!');
   let sum = 0;
   for (let i = fixedStartNumber; i<= fixedFinishNumber; i++){
     if (i%2 === 0){
@@ -22,8 +24,41 @@ if (passOrNot === false) {
     }
     sum+=i;
   }
-  console.log(sum);
-} 
+  document.writeln(`Your numbers sum is: ${sum}`);
+}
+} else {
+  alert('Entered data is not a number!!! Enter numbers, please:)');
+}} else {
+  document.writeln(':(((( Come again to play! ;)')
+}
+
+
+
+
+
+
+
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          
+
 
 
 
