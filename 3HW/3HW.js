a8247b1c5e757a144aebbdb7e9afec28c2071b0c
Oksana Kuliesh <okaresta1@gmail.com>
2020-02-18 16:35:55 +0200

    fifth function modification

--- 3HW/3HW.js
+++ 3HW/3HW.js
@@ -1,62 +1,57 @@
 //1
 
-function getMaxDigit (num) {
+function getMaxDigit(num) {
     let result = 0;
-  for (let i = num; i>0; i = Math.floor(i/10)) {
-    let digit = i % 10;
-    if (result < digit)
-      result = digit;
-  }
-  return result;
+    for (let i = num; i > 0; i = Math.floor(i / 10)) {
+        let digit = i % 10;
+        if (result < digit)
+            result = digit;
+    }
+    return result;
 }
 
 
 //2
- function getPow (num, exp) {
-  let result = 1;
+function getPow(num, exp) {
+    let result = 1;
     for (let i = 1; i <= exp; i++) {
         result *= num;
-   }
-  return result;
+    }
+    return result;
 }
 
 
 //3
-function editName (name) {
- const edited = name.charAt(0).toUpperCase() + name.substr(1).toLowerCase();
- return edited;
+function editName(name) {
+    return name.charAt(0).toUpperCase() + name.substr(1).toLowerCase();
 }
 
 
 //4
-function findPureSalary (salary){
-  const tax1 = (salary/100)*18;
-  const tax2 = (salary/100)*1.5;
-  const pureSalary = salary - tax1 - tax2;
-  return pureSalary;
+function findPureSalary(salary) {
+    const tax1 = (salary / 100) * 18;
+    const tax2 = (salary / 100) * 1.5;
+    const pureSalary = salary - tax1 - tax2;
+    return pureSalary;
 }
 
 
 //5
-function convertMoney (string) {
-  let result = 'We don\'t have a result for you(((';
-  const exRate = 24;
-  if(string.endsWith('UAH') === true) {
-    let sum = string.replace('UAH', '');
-    converted = +sum/exRate;
-    result = 'Your result is: ' + converted + ' USD'; 
-  } else if (string.endsWith('uah') === true) {
-    let sum = string.replace('uah', '');
-    converted = +sum/exRate;
-    result = 'Your result is: ' + converted + ' USD';
-  } else if (string.endsWith('$') === true) {
-    //let index$ = string.indexOf('$'); //3
-    let sum = string.replace('$','');
-    converted = sum * exRate;
-    result = 'Your result is: ' + converted + ' UAH';
-  } else {
-    console.log('Wrong currency!Improve currency, please');
-  }
+function convertMoney(string) {
+    let result = 'We don\'t have a result for you(((';
+    const exRate = 24;
+    let lowStr = string.toLowerCase();
+    if (lowStr.endsWith('uah') === true) {
+        let sum = lowStr.replace('uah', '');
+        let converted = +sum / exRate;
+        result = 'Your result is: ' + converted + ' USD';
+    } else if (string.endsWith('$') === true) {
+        let sum = string.replace('$', '');
+        converted = sum * exRate;
+        result = 'Your result is: ' + converted + ' UAH';
+    } else {
+        console.log('Wrong currency!Improve currency, please');
+    }
     return result;
 }
 
@@ -69,5 +64,3 @@ console.log(`Function №1: ${getMaxDigit (123456789)}
  Function №4: ${findPureSalary (1000)}
  Function №5: ${convertMoney ('12000 UAH')}
  Function №6: ${isPlndrm('MaDam')}`);
-
-
